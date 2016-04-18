8f7bbb853847f183860f2090d96b97319c787a6c
charles carter <charles.h.carter@gmail.com>
2016-04-18 09:58:43 -0700

    display toast on findREstaurantsButton click

--- app/src/main/java/com/example/guest/ourrestaurant/MainActivity.java
+++ app/src/main/java/com/example/guest/ourrestaurant/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.guest.ourrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+            mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "Toasty", Toast.LENGTH_LONG).show();
+
+                }
+            });
     }
 }
