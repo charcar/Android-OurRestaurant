3b0d57e45f67acef4aaf9c932d7da8ddc5bc0881
Charlie Carter <charles.h.carter@gmail.com>
2016-04-25 10:29:07 -0700

    integrating yelp api

--- app/src/main/java/com/example/guest/ourrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/example/guest/ourrestaurant/RestaurantsActivity.java
@@ -3,7 +3,14 @@ package com.example.guest.ourrestaurant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
@@ -18,6 +25,27 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near:" + location);
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        YelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
