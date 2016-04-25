de9f17786fd383035081844a10a9f57fac54ce13
Charlie Carter <charles.h.carter@gmail.com>
2016-04-25 09:16:48 -0700

    integrate yelp api w/ properly obfuscated access tokens

---
+++ app/src/main/java/com/example/guest/ourrestaurant/Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.ourrestaurant;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
